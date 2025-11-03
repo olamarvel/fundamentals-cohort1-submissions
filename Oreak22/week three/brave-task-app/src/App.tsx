a147d1767e500b89ba96942be7555f2eab0f8ae6
Rejoice Obe <rejoiceobe235@gmail.com>
2025-11-03 19:54:05 +0100

    Added week one to week five

---
+++ Oreak22/week three/brave-task-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./Pages/auth/Login";
+
+function App() {
+  return (
+    <>
+      {/* <Routes> */}
+        {/* // authentication */}
+        {/* <Route path="/auth">
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes> */}
+      hello
+    </>
+  );
+}
+
+export default App;
